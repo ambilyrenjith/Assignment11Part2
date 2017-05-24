9a77d3527443570b20a1aac2ca350ff3dee8d508
ambilyrenjith <ambilyrenjith@yahoo.com>
2017-05-23 18:24:39 -0700

    Final commit

--- Part 2/directory/js/script1.js
+++ Part 2/directory/js/script1.js
@@ -1,8 +1,13 @@
 
+
 $(document).ready(function () {
+    
     $("#mySearch").keypress(function(e) {
+        $("h1").hide();
+        $("#container p").hide();
         var v = callMeOnKeyPress(e.key);
     });
+    
 });
 
 
@@ -25,7 +30,36 @@ function callMeOnKeyPress(inputData) {
         
         }).done(function() {
             console.log("success");
-            var v = matchedRecords;
-         });
+            
+            $("#container").append('<ul id="list" data-role="listview"></ul>');
+                
+                for(var i=0;i<matchedRecords.length;i++)
+                 {    
+                    $("#list")
+                        .append("<li><img src=" + matchedRecords[i].ImagePath +">" +matchedRecords[i].name+"<li>");
+                        /*.append("<li>" + matchedRecords[i].title + "</li>")
+                        .append("<li>" + matchedRecords[i].name + "</li>");
+                      */
+        
+                    $("#list").listview();
+                 }
+          
+           /* $("#list" '<li>').append("<li><img src=" + matchedRecords[0].ImagePath +"> +"+ matchedRecords[0].title+matchedRecords[0].name +" <li>");
+            $("#list").listview();
+        
+        */
+        });
 }
+     
+    
+        
+        
+
+
+   	
+    
+
+
+
+
 
